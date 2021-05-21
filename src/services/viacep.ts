0097b326064090f2d31447d3cfd1ba75d56a8252
DESKTOP-JVIJUL5\55659 <mayaramaaraujo98@gmail.com>
2021-05-20 21:05:43 -0300

    remove unremoving unused code

--- src/services/viacep.ts
+++ src/services/viacep.ts
@@ -5,16 +5,10 @@ const BASE_URL = "viacep.com.br/ws"
 export class ViaCep {
 
   public async getAddress(cep: number) {
-    // axios.get(`${BASE_URL}/${cep}/json`)
-    //   .then(res => console.log(res))
-    //   .catch(err => console.log(err))
     try {
       const res = await axios.get(`${BASE_URL}/${cep}/json`)
-      console.log(res)
-
-      return res.data
+      return res
     } catch (error) {
-      console.log(error)
       return error
     }
   }
